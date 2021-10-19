06284960a5c68eb60c38c7231d0c443d60d0b8f6
StormFlaate <fflaate@hotmail.no>
2021-10-19 20:34:51 +0200

    Fixing the order of weights and loss for all functions

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -5,43 +5,37 @@ from proj1_helpers import logistic_regression_gradient_descent_one_step, compute
 from proj1_helpers import reguralized_logistic_regression_one_step
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
-    ws = [initial_w]
-    losses = []
     w = initial_w
+    loss = compute_loss(y,tx,w)
     for n_iter in range(max_iters):
         loss = compute_loss(y,tx,w)
         gradient = compute_gradient(y,tx,w)
         w = w - gamma*gradient
-        ws.append(w)
-        losses.append(loss)
 
-    return losses, ws
+    return w, loss
 
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     batch_size = 1  # Change this maybe
-    ws = [initial_w]
-    losses = []
     w = initial_w
-    
+    loss = compute_loss(y,tx,w)
     for y_n, tx_n in batch_iter(y, tx, batch_size, max_iters): # Import function
         loss = compute_loss(y_n,tx_n,w)
         stoch_gradient = compute_stoch_gradient(y_n, tx_n, w)
         w = w - gamma*stoch_gradient
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
-    return ws, losses, 
+        
+    return w, loss
 
 def least_squares(y, tx):
     #w = np.linalg.inv((tx.T)@(tx))@((tx.T)@y)
     w = np.linalg.lstsq(tx,y)[0]
     N = y.shape[0] # Number of elements in vector y
     error = y-(tx@w)
-    mse = (1/(2*N))*(error.T @ error)
+    loss = (1/(2*N))*(error.T @ error)
     
-    return mse, w
+    return w, loss
 
+"""
 def ridge_regression(y, tx, lambda_):
     N = y.shape[0] # Number of elements in vector y
     XTX = tx.T@tx 
@@ -49,7 +43,7 @@ def ridge_regression(y, tx, lambda_):
     XTy = (tx.T @ y)
     w = (np.linalg.inv(XTX+lambda_marked)) @ XTy #Taking inverse and matmult with second term
     return w
-
+"""
 
 def ridge_regression(y, tx, lambda_): 
     N = tx.shape[0] # Collecting the number of rows in tx
@@ -63,7 +57,7 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma_):
     w = initial_w # Setting the initial w to w
     for i in range(max_iters):
         # Loops max_iters number of times, getting last loss and last w each time
-        loss, w = logistic_regression_gradient_descent_one_step(y, tx, w, gamma_)
+        w, loss = logistic_regression_gradient_descent_one_step(y, tx, w, gamma_)
     return w, loss
 
 
@@ -71,6 +65,6 @@ def reguralized_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma_
     w = initial_w # Setting the initial w to w
     for i in range(max_iters):
         # Loops max_iters number of times, getting last loss and last w each time
-        loss, w = reguralized_logistic_regression_one_step(y, tx, w, gamma_, lambda_)
+        w, loss = reguralized_logistic_regression_one_step(y, tx, w, gamma_, lambda_)
     return w, loss
 
